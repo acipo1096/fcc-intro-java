dd229f17db7a0ca2d1b1420edf0dbcb92485e257
Alex Cipollone <alex.cipollone10@gmail.com>
2024-01-10 14:39:09 -0500

    began learning Maps

---
+++ tutorial1/src/Maps_HashMaps/Maps.java
@@ -0,0 +1,11 @@
+package Maps_HashMaps;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Maps {
+    public static void main(String[] args) {
+        // Think of a Map like a Python dictionary
+        // A MAP is a key, value pair
+        Map m = new HashMap();
+    }
+}
